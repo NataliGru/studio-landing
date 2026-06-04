6a917dcca93d09716557c847d55133f952fb1e7c
Nataliia Hrushanyk <nataligrushanyk@gmail.com>
2026-06-04 14:46:23 +0300

    add imgs

--- src/shared/components/smoothScrollLensis/index.tsx
+++ src/shared/components/smoothScrollLensis/index.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-
 import ReactLenis from 'lenis/react';
 
 import { ParallaxSection } from '@/shared/ui/parallax/parallaxSection';
@@ -11,40 +9,42 @@ export const SmoothScrollLenis = () => {
         <ParallaxSection
           stickyContent={
             <div className='flex h-full items-center justify-center bg-black'>
-              <h1 className='text-8xl text-white'>Nataliia</h1>
+              {/* <h1 className='text-8xl text-white'>Nataliia</h1> */}
             </div>
           }
-          items={[
-            {
-              id: 'image-1',
-              start: -200,
-              end: 200,
-              className: 'ml-auto w-1/3',
-              content: (
-                <div className='flex h-full items-center justify-center bg-black'>
-                  <h1 className='text-8xl text-white'>Nataliia</h1>
-                </div>
-              ),
-            },
-            {
-              id: 'image-2',
-              start: 200,
-              end: -200,
-              className: 'ml-auto w-1/3',
-              content: (
-                <div className='flex h-full items-center justify-center bg-black'>
-                  <h1 className='text-8xl text-white'>Nataliia222</h1>
-                </div>
-              ),
-            },
-          ]}
+          items={
+            [
+              // {
+              //   id: 'image-1',
+              //   start: -200,
+              //   end: 200,
+              //   className: 'ml-auto w-1/3',
+              //   content: (
+              //     <div className='flex h-full items-center justify-center bg-black'>
+              //       <h1 className='text-8xl text-white'>Nataliia</h1>
+              //     </div>
+              //   ),
+              // },
+              // {
+              //   id: 'image-2',
+              //   start: 200,
+              //   end: -200,
+              //   className: 'ml-auto w-1/3',
+              //   content: (
+              //     <div className='flex h-full items-center justify-center bg-black'>
+              //       <h1 className='text-8xl text-white'>Nataliia222</h1>
+              //     </div>
+              //   ),
+              // },
+            ]
+          }
         />
 
         <ParallaxSection
           stickyRevealEffects={{ clip: true, scale: true }}
           stickyContent={
             <div className='relative h-full w-full'>
-              <Image
+              {/* <Image
                 src='/4.jpg'
                 fill
                 alt=''
@@ -52,46 +52,48 @@ export const SmoothScrollLenis = () => {
                 fetchPriority='high'
                 preload
                 sizes='100vw'
-              />
+              /> */}
             </div>
           }
-          items={[
-            {
-              id: 'image-1',
-              start: -200,
-              end: 200,
-              className: 'ml-auto w-1/3',
-              content: (
-                <Image
-                  src='/1.jpg'
-                  alt='image 1'
-                  width={400}
-                  height={500}
-                  className='h-auto w-full object-cover'
-                  loading='lazy'
-                  sizes='(max-width: 768px) 100vw, 33vw'
-                />
-              ),
-            },
-            {
-              id: 'image-2',
-              start: 0,
-              end: -500,
-              className: 'ml-24 w-5/12',
-              content: (
-                <Image
-                  src='/2.jpg'
-                  alt='image 2'
-                  width={500}
-                  height={650}
-                  className='h-auto w-full object-cover'
-                  fetchPriority='high'
-                  preload
-                  sizes='(max-width: 768px) 100vw, 42vw'
-                />
-              ),
-            },
-          ]}
+          items={
+            [
+              // {
+              //   id: 'image-1',
+              //   start: -200,
+              //   end: 200,
+              //   className: 'ml-auto w-1/3',
+              //   content: (
+              //     <Image
+              //       src='/1.jpg'
+              //       alt='image 1'
+              //       width={400}
+              //       height={500}
+              //       className='h-auto w-full object-cover'
+              //       loading='lazy'
+              //       sizes='(max-width: 768px) 100vw, 33vw'
+              //     />
+              //   ),
+              // },
+              // {
+              //   id: 'image-2',
+              //   start: 0,
+              //   end: -500,
+              //   className: 'ml-24 w-5/12',
+              //   content: (
+              //     <Image
+              //       src='/2.jpg'
+              //       alt='image 2'
+              //       width={500}
+              //       height={650}
+              //       className='h-auto w-full object-cover'
+              //       fetchPriority='high'
+              //       preload
+              //       sizes='(max-width: 768px) 100vw, 42vw'
+              //     />
+              //   ),
+              // },
+            ]
+          }
         />
       </ReactLenis>
     </div>
